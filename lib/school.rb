ab6725a44efd617a8e0f1178c83422966139ce6d
Di Zheng <dizhengcindy@gmail.com>
2020-03-11 05:25:41 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,28 @@
 # code here!
+class School
+  attr_accessor :name,:roster
+def initialize(name)  
+  @name = name
+  @roster = {}
+end
+  
+  def add_student(name,num)
+    roster[num] ||=[]
+    roster[num] << name
+  end
+  
+  def grade(num)
+    roster[num]
+    
+  end
+  def sort 
+    newHash ={}
+   roster.each{|k,v| newHash[k]=v.sort}
+   newHash
+  end
+  
+  
+  
+  
+  
+end
